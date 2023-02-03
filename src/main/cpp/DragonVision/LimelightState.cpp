25e0bec6282aa098fe0e464231572aae63b825c0
Nick Frisch <nfrisch3@outlook.com>
2023-02-03 14:29:37 -0500

    Clean up unnecessary variable

--- src/main/cpp/DragonVision/LimelightState.cpp
+++ src/main/cpp/DragonVision/LimelightState.cpp
@@ -1,10 +1,22 @@
-
+//====================================================================================================================================================
+// Copyright 2022 Lake Orion Robotics FIRST Team 302 
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+// OR OTHER DEALINGS IN THE SOFTWARE.
+//====================================================================================================================================================
 
 #include <DragonVision/LimelightState.h>
 
 using namespace nt;
 using namespace std;
-
 
 LimelightState::LimelightState
 (
@@ -19,10 +31,9 @@ LimelightState::LimelightState
 
 bool LimelightState::HasTarget() const
 {
-   auto nt = m_networktable.get();
-    if (nt != nullptr)
+    if (m_networktable != nullptr)
     {
-        return ( nt->GetNumber("tv", 0.0) > 0.1 );
+        return ( m_networktable->GetNumber("tv", 0.0) > 0.1 );
     }
     return false;
 }
@@ -73,17 +84,15 @@ units::angle::degree_t LimelightState::GetTargetVerticalOffset() const
 
 double LimelightState::GetTargetArea() const
 {
-    auto nt = m_networktable.get();
-    if (nt != nullptr)
+    if (m_networktable != nullptr)
     {
-        return nt->GetNumber("ta", 0.0);
+        return m_networktable->GetNumber("ta", 0.0);
     }
     return 0.0;
 }
 
 units::angle::degree_t LimelightState::GetTargetSkew() const
 {
- //   auto nt = m_networktable.get();
     if (m_networktable != nullptr)
     {
         return units::angle::degree_t(m_networktable->GetNumber("ts", 0.0));
@@ -93,10 +102,9 @@ units::angle::degree_t LimelightState::GetTargetSkew() const
 
 units::time::microsecond_t LimelightState::GetPipelineLatency() const
 {
-    auto nt = m_networktable.get();
-    if (nt != nullptr)
+    if (m_networktable != nullptr)
     {
-        return units::time::second_t(nt->GetNumber("tl", 0.0));
+        return units::time::second_t(m_networktable->GetNumber("tl", 0.0));
     }
     return units::time::second_t(0.0);
 }
