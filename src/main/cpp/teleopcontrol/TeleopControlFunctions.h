2a2239269b1cb602bbb9fa71a4aada655ea3185a
Nick Frisch <nfrisch3@outlook.com>
2023-02-08 20:59:17 -0500

    Start moving grabber over to 1 solenoid

--- src/main/cpp/teleopcontrol/TeleopControlFunctions.h
+++ src/main/cpp/teleopcontrol/TeleopControlFunctions.h
@@ -1,6 +1,6 @@
 
 //====================================================================================================================================================
-// Copyright 2022 Lake Orion Robotics FIRST Team 302 
+// Copyright 2022 Lake Orion Robotics FIRST Team 302
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
 // to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
@@ -14,7 +14,7 @@
 // OR OTHER DEALINGS IN THE SOFTWARE.
 //====================================================================================================================================================
 
-#pragma once 
+#pragma once
 
 // C++ Includes
 
@@ -24,70 +24,66 @@
 
 class TeleopControlFunctions
 {
-    public:
-        enum FUNCTION
-        {
-            UNKNOWN_FUNCTION,
-            ROBOT_ORIENTED_DRIVE,
-            HOLONOMIC_DRIVE_FORWARD,
-            HOLONOMIC_DRIVE_ROTATE,
-            HOLONOMIC_DRIVE_STRAFE,
-            HOLONOMIC_ROTATE_FRONT,
-            HOLONOMIC_ROTATE_BACK,
-            HOLONOMIC_ROTATE_LEFT,
-            HOLONOMIC_ROTATE_RIGHT,
-            REZERO_PIGEON,
-            HOLD_POSITION,
-            FINDTARGET,        
-            DRIVE_TO_SHOOTING_SPOT,
-            ARCADE_THROTTLE,
-            ARCADE_STEER,            
-            DRIVE_TO_WALL_GRID,
-            DRIVE_TO_COOP_GRID,
-            DRIVE_TO_HP_GRID,            
-            DRIVE_TO_HUMAN_PLAYER_RIGHT,
-            DRIVE_TO_HUMAN_PLAYER_LEFT,
-            BALANCE_MODE,
-            AUTO_BALANCE,
-             //Arm states
-            HOLD_POSITION_ROTATE,
-            MANUAL_ROTATE,
-            CUBE_BACKROW_ROTATE,
-            CONE_BACKROW_ROTATE,
-            CUBE_MIDROW_ROTATE,
-            CONE_MIDROW_ROTATE,
-            HUMAN_PLAYER_STATION_ROTATE,
-            STARTING_POSITION_ROTATE,
-            FLOOR_POSITION_ROTATE,
-            //Extender states
-            HOLD_POSITION_EXTEND,
-            MANUAL_EXTEND_RETRACT,
-            CUBE_BACKROW_EXTEND,
-            CONE_BACKROW_EXTEND,
-            CUBE_MIDROW_EXTEND,
-            CONE_MIDROW_EXTEND,
-            HUMAN_PLAYER_STATION_EXTEND,
-            STARTING_POSITION_EXTEND,
-            FLOOR_EXTEND,
-            FRONT_LEFT_BUMPER_TURNABOUT_POINT,
-            FRONT_RIGHT_BUMPER_TURNABOUT_POINT,
-            //Grabber states
-            OPEN,
-            GRABBING_CONE,
-            GRABBING_CUBE,
-            HOLDING_CONE,
-            HOLDING_CUBE,
-            RELEASE,
-            CYCLE_GRABBER,
-            //Active intake controls?
-            INTAKE, 
-            EXPEL,
-            //notcharlie states
-            STARTING_POSITION,
-            LOW,
-            MED,
-            HIGH,
-            HUMAN_PLAYER_LEVEL,
-            MAX_FUNCTIONS
-        };
+public:
+    enum FUNCTION
+    {
+        UNKNOWN_FUNCTION,
+        ROBOT_ORIENTED_DRIVE,
+        HOLONOMIC_DRIVE_FORWARD,
+        HOLONOMIC_DRIVE_ROTATE,
+        HOLONOMIC_DRIVE_STRAFE,
+        HOLONOMIC_ROTATE_FRONT,
+        HOLONOMIC_ROTATE_BACK,
+        HOLONOMIC_ROTATE_LEFT,
+        HOLONOMIC_ROTATE_RIGHT,
+        REZERO_PIGEON,
+        HOLD_POSITION,
+        FINDTARGET,
+        DRIVE_TO_SHOOTING_SPOT,
+        ARCADE_THROTTLE,
+        ARCADE_STEER,
+        DRIVE_TO_WALL_GRID,
+        DRIVE_TO_COOP_GRID,
+        DRIVE_TO_HP_GRID,
+        DRIVE_TO_HUMAN_PLAYER_RIGHT,
+        DRIVE_TO_HUMAN_PLAYER_LEFT,
+        BALANCE_MODE,
+        AUTO_BALANCE,
+        // Arm states
+        HOLD_POSITION_ROTATE,
+        MANUAL_ROTATE,
+        CUBE_BACKROW_ROTATE,
+        CONE_BACKROW_ROTATE,
+        CUBE_MIDROW_ROTATE,
+        CONE_MIDROW_ROTATE,
+        HUMAN_PLAYER_STATION_ROTATE,
+        STARTING_POSITION_ROTATE,
+        FLOOR_POSITION_ROTATE,
+        // Extender states
+        HOLD_POSITION_EXTEND,
+        MANUAL_EXTEND_RETRACT,
+        CUBE_BACKROW_EXTEND,
+        CONE_BACKROW_EXTEND,
+        CUBE_MIDROW_EXTEND,
+        CONE_MIDROW_EXTEND,
+        HUMAN_PLAYER_STATION_EXTEND,
+        STARTING_POSITION_EXTEND,
+        FLOOR_EXTEND,
+        FRONT_LEFT_BUMPER_TURNABOUT_POINT,
+        FRONT_RIGHT_BUMPER_TURNABOUT_POINT,
+        // Grabber states
+        OPEN,
+        GRAB,
+        CYCLE_GRABBER,
+        // Active intake controls?
+        INTAKE,
+        EXPEL,
+        // notcharlie states
+        STARTING_POSITION,
+        LOW,
+        MED,
+        HIGH,
+        HUMAN_PLAYER_LEVEL,
+        MAX_FUNCTIONS
+    };
 };
