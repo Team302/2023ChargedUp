0b0ecd919dab93f0d20c0e9891dff9404929752e
Tnaay Patel <adambots.tanay@gmail.com>
2023-02-12 23:31:46 -0500

    Moved logging to check atTarget
    Also had a build issue in dragonVision. needed to update include

--- src/main/cpp/mechanisms/arm/ArmStateMgr.cpp
+++ src/main/cpp/mechanisms/arm/ArmStateMgr.cpp
@@ -126,6 +126,9 @@ void ArmStateMgr::CheckForGamepadTransitions()
             {
                 CheckForCubeGamepadTransitions(controller);
             }
+
+            Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, string("ArmMgr"), string("Target"), m_arm->GetTarget());
+            Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, string("ArmMgr"), string("Position (Degrees)"), m_arm->GetPositionDegrees().to<double>());
             // If arm is at target and the prev state hasn't changed then stay in hold
             if (abs(m_arm->GetPositionDegrees().to<double>() - m_arm->GetTarget()) < 1.0 && m_arm->GetPositionDegrees().to<double>() > 1.0 && m_prevState == m_targetState)
             {
@@ -255,9 +258,6 @@ void ArmStateMgr::CheckForStateTransition()
             }
         }
     }
-
-    Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, string("ArmMgr"), string("Target"), m_arm->GetTarget());
-    Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, string("ArmMgr"), string("Position (Degrees)"), m_arm->GetPositionDegrees().to<double>());
 }
 
 void ArmStateMgr::Update(RobotStateChanges::StateChange change, int state)
