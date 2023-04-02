e473516890f5e1017f348f295ddc3d7fbc993d72
Tanay Patel <adambots.tanay@gmail.com>
2023-04-02 12:53:50 -0400

    Updating paths

--- src/main/cpp/chassis/swerve/driveStates/TrajectoryDrivePathPlanner.cpp
+++ src/main/cpp/chassis/swerve/driveStates/TrajectoryDrivePathPlanner.cpp
@@ -29,7 +29,7 @@ TrajectoryDrivePathPlanner::TrajectoryDrivePathPlanner(RobotDrive *robotDrive) :
                                                                                  m_robotDrive(robotDrive),
                                                                                  m_holonomicController(frc2::PIDController{0.35, 0.0, 0}, // 3.725, 0.85
                                                                                                        frc2::PIDController{0.35, 0.0, 0}, // 3.725, 0.85
-                                                                                                       frc::PIDController{1.65, 0.0, 0}), // 0.325, 0.0
+                                                                                                       frc::PIDController{1.6, 0.0, 0}),  // 0.325, 0.0
                                                                                  m_desiredState(),
                                                                                  m_trajectoryStates(),
                                                                                  m_prevPose(ChassisFactory::GetChassisFactory()->GetSwerveChassis()->GetPose()),
